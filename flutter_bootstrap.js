06bc5757958111863bc390cac4d5aff5d7b06e38
João Felipe <joaofelipedev01@gmail.com>
2025-02-19 09:16:37 -0300

    Update service worker version and related asset references

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1511069898"
+    serviceWorkerVersion: "2399846060"
   }
 });
